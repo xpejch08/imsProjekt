6b7535626ec0cc72d4d1ea9cb9f947e39bfb3a93
ondrej <ondra.ceska@gmail.com>
2023-12-08 22:20:44 +0100

    added arguments support (AAAAARGH), second test experiment with 1 infected

--- main.cpp
+++ main.cpp
@@ -1,161 +1,94 @@
-#include <simlib.h>
-#include <iostream>
-#include <stdio.h>
+#include <vector>
+#include "simlib.h"
+#include <cstring>
 
-int oldestPig = 0;
-class Pig : public Process {
-    bool isFemale;
-    bool isAlive = true;
-    int pigIndex;
-    int litterCount = 0;
-    int expecting;
-    bool prohibited = false;
-    int prohibitedexpire;
-    double expecteddeliveryday;
-    int age;
-    void Behavior() {
-        if(0==Time){ //31.3.
-            if(Random()<0.40733661){ //theres more under 1 years old than adult pigs (sele)
-                age = int(Uniform(365,3650));
-            }else{
-                age = int(Uniform(0,365));
-            }
+int getParams(int argc, char *argv[]){
+    if (argc<2){
+        return 1;
+    }
+    if (strcmp(argv[1],"exp1")==0){
+        return 1;
+    }else if((strcmp(argv[1],"exp2")==0)){
+        return 2;
+    }else if((strcmp(argv[1],"exp3")==0)){
+        return 3;
+    }else if((strcmp(argv[1],"exp4")==0)){
+        return 4;
+    }else if((strcmp(argv[1],"exp5")==0)){
+        return 5;
+    }
+    return 1;
+}
 
-        }else{
-            age=0;
-            prohibited = true;
-            prohibitedexpire = 90; //pigs under 3 months are prohibited to shoot (asi trosku bullshit :) )
-        }
-        if(Random() < 0.45){ //pigs are more likely to be male
-            isFemale = true;
-        } else {
-            isFemale = false;
-        }
-        // Pigs live 10 years
-        while(age < 3650) {
-            age++;
-            if(oldestPig < pigIndex){
-                oldestPig = pigIndex;
-                std::cout << "PigIndex: " << pigIndex << "Female pig, Age: " << age << "PigCount: " << pigAmount << std::endl;
-            }
-            if (isAlive && prohibitedexpire==age){ //no longer protected from hunters
-                prohibited = false;
-                prohibitedexpire = 0;
-            }
 
-            if (isAlive && expecteddeliveryday == age){
-                for(int i =0; i< expecting;++i){
-                    (new Pig)->Activate(); //new offspring is born when mother is still alive and his birth has come
-                    pigAmount++;
-                    std::cout << "born" << std::endl;
-                }
-                expecteddeliveryday = 0;
-                expecting = 0;
-            }
-            if (isAlive && isFemale && age >= 240 && age <= 730 && expecting==0) { // Females reproduce between 8-24 months
-                if (Random() < 0.6 && isInBreedingSeason() && (litterCount < 2)) {
-                    litterCount++;
-                    std::cout << "intercourse" << std::endl;
-                    int offspring = int(Normal(7.5, 2.5)); // Average litter size
-                    for(int i = 0; i <= offspring; ++i) {
-                        expecting++;
-                    }
-                    expecteddeliveryday = age + 120; //planned birth date
-                    prohibited = true;
-                    prohibitedexpire = age + 120 + 485; //piglets are with their mother for 18 months, mothers with pigs are protected from hunters
-                }
-            }
-            if( isAlive && !prohibited && Random() < 0.008423402){  //  ( 184328 / 59953 / 365) 307% chance of getting shot each year
-                if(isAlive){
-                    pigAmount--;
-                }
-                std::cout << "shot" << std::endl; // dying from being shot
-                Terminate(); // pig died
-                isAlive = false;
-            }
-            if(age % 365 == 0){ //can be pregnant again
-                litterCount = 0;
-            }
-            // dying
-            if (isAlive && Random() < 0.0002886172890222971) { // 10%chance dying each year
-                if(isAlive){
-                    pigAmount--;
-                }
-                std::cout << "natural causes" << std::endl;
-                Terminate(); // pig died
-                isAlive = false;
-            }
+struct SIRModel {
 
-            // next birth cycle
-            Wait(1);
-        }
-        Terminate();
-        isAlive = false;
 
+    Parameter beta;  // Infection rate
+    Parameter gamma; // Recovery rate
+    double N;        // Total population
+
+    Integrator S; // Susceptible population
+    Integrator I; // Infected population
+    Integrator R; // Recovered population
+
+    // constructor
+    SIRModel(double beta_value, double gamma_value, double N_value, double S0, double I0, double R0) :
+            beta(beta_value), gamma(gamma_value), N(N_value),
+            S(-beta * S * I / N, S0), //equation for s
+            I(beta * S * I / N - gamma * I, I0), //equation for i
+            R(gamma * I, R0) {} //equation for r
+
+    void updateModel(double beta_value, double gamma_value, double N_value, double S0, double I0, double R0) {
+        beta = beta_value;
+        gamma = gamma_value;
+        N = N_value;
+
+        // Initialize the Integrators with new values
+        S.Init(S0);
+        I.Init(I0);
+        R.Init(R0);
     }
 
-    bool isInBreedingSeason() {
-        int dayOfYear = age % 365;
-        return ((dayOfYear >= 305 || dayOfYear <=31) || ((dayOfYear <= 122 || dayOfYear >= 275) && Random() < 0.005));
-        //slight chance of mating outside main mating months
-
-    }
-public:
-    Pig() : pigIndex(nextPigIndex++) { // Assign a unique index to each pig
-    }
-    static int pigAmount;
-    static int nextPigIndex;
 };
+//load conditions based on experiment
+std::vector<double> loadConditions(int x){
+    // Initial conditions
+    double initial_susceptible = 730; // 730 people who have the potential to be infected, according to the table
+    double initial_infected = 270;     // 270 infected
+    double initial_recovered = 0;    // 0recovered for start
+    // Total population
+    double total_population = 1000;
+    // Model parameters
+    double transmission_rate = 0.3;  // Beta
+    double recovery_rate = 0.43;      // Gamma
 
-int Pig::pigAmount = 61915; //pocet prasat 2023
-int Pig::nextPigIndex = 0;
-
-//class PigPopulation : public Process {
-//    void Behavior() {
-//        int simTime = 0;
-//        while (simTime < 365 ) {
-//            simTime--;
-//            // Podmínky pro rozmnožování prasat
-//            if (shouldReproduce()) {
-//                int numberOfNewPigs = removeSomePigsFromPopulation(calculateNumberOfNewPigs());
-//                for (int i = 0; i < numberOfNewPigs; ++i) {
-//                    (new Pig)->Activate(Time + Exponential(365)); // Aktivace nových prasat v populaci
-//                }
-//            }
-//
-//
-//            // Čekání na další událost (např. další rok)
-//            Wait(Exponential(365)); // Například 365 dní pro simulaci jednoho roku
-//        }
-//    }
-//    bool shouldReproduce() {
-//        // Zde implementujte logiku, která určuje, zda prasata mohou rozmnožovat
-//        return true; // Příklad: vždy vrací true
-//    }
-//
-//    int calculateNumberOfNewPigs() {
-//        // Zde implementujte logiku pro výpočet počtu nových prasat
-//        return Normal(10, 2); // Příklad: průměrně 10 nových prasat s nějakou variabilitou
-//    }
-//
-//    bool shouldSomePigsDie() {
-//        // Zde implementujte logiku, která určuje, zda by některá prasata měla umřít
-//        return Random() < 0.05; // Příklad: 5% šance na úmrtnost v populaci
-//    }
-//    int removeSomePigsFromPopulation(int removeSome){
-//        return removeSome - Normal(10, 2);
-//    }
-//};
-
-int main() {
-    Init(0, 365 );  // Inicializace simulace na 10 let
-    for(int i = 0; i < Pig::pigAmount; ++i) {  // Předpokládejme počáteční populaci 50 prasat
-        (new Pig)->Activate();
+    if(x==2) { //experiment 2
+        initial_infected = 1;     // 1 infected
     }
-    Run();  // Spuštění simulace
+    return {transmission_rate, recovery_rate, total_population, initial_susceptible, initial_infected, initial_recovered};
+}
+// Instantiate the SIR model
+SIRModel model(0.3,0.43,1000,730,270,0);
+
+// Function to output the results
+void Sample() {
+    Print("%g %g %g %g\n", T.Value(), model.S.Value(), model.I.Value(), model.R.Value());
+
+}
+Sampler s(Sample, 1);
+
+int main(int argc, char *argv[]) {
+    std::vector <double> conds;
+    conds = loadConditions(getParams(argc,argv));
+    SetOutput("population.dat");
+    Print("# SIR Model\n");
+    model.updateModel(conds[0], conds[1], conds[2], conds[3], conds[4], conds[5]);
 
 
-    std::cout << "Počet prasat po roce: " << Pig::pigAmount << std::endl;
 
-    SIMLIB_statistics.Output();
+    Init(0, 3650);  // 100 time units
+    SetStep(1e-6, 0.1);
+    Run();
+    return 0;
 }
